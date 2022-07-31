bbdd184c9891002d11af95dbe50e3c4e34e87a53
Aleksandr Minayev <tenshisasha@gmail.com>
2022-07-31 14:35:05 +0300

    Made repository, dtos and controller for Product

---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext dbContext;
+
+        public ProductRepository(ShopOnlineDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await dbContext.ProductCategories.ToListAsync();
+
+            return categories;
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await dbContext.Products.ToListAsync();
+
+            return products;
+        }
+    }
+}
